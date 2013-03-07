4f6a283484d89174d9ff16dd1d7228f0e0833183
MrBrdo <mrbrdo@gmail.com>
2013-03-07 02:35:52 +0100

    bare rails app without changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarionetteRails::Application.config.secret_token = '44e4369ca1f82bd7f9dc454942e79c490487434e9b132aac3505bde1c136432128b20840d4e576e6bb5cbe9f4c6355ea50c81a0061672f8ff3656c3f29e8216f'
